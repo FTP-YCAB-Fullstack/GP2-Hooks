df96120a7a2da45de8f3fc4730a169bffb14926a
GanXx8 <ganda.imasxx@gmail.com>
2021-09-03 11:12:23 +0700

    Repair Skeleton Route

--- hooks/src/page/CryptoCurrency.jsx
+++ hooks/src/page/CryptoCurrency.jsx
@@ -1,7 +1,9 @@
+import Navbar from "../Component/Navbar";
 
 function CryptoCurrency(){
     return(
         <div>
+            <Navbar />
             <h1>Ini Crypto Currency</h1>
             <h4>Hello World</h4>
         </div>
