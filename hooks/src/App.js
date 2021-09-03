5382fa93d9aac14dd8276f86482f60cfad6931e9
hamdiabraham <hamdiabraham.kh@gmail.com>
2021-09-03 20:13:40 +0700

    first commit

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,25 +1,24 @@
 import Home from "./page/Home";
 import CurrencyConverter from "./page/CurrencyConverter";
 import CryptoCurrency from "./page/CryptoCurrency";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route 
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-    <Router> 
-             <Switch>
-                 <Route path="/" exact component={Home}/>
-                 <Route path="/Home" exact component={Home}/>
-                 <Route path="/CurrencyConverter" exact component={CurrencyConverter}/>
-                 <Route path="/CryptoCurrency" exact component={CryptoCurrency}/> 
-             </Switch>
-  </Router>
-    
-   </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Home" exact component={Home} />
+          <Route
+            path="/CurrencyConverter"
+            exact
+            component={CurrencyConverter}
+          />
+          <Route path="/CryptoCurrency" exact component={CryptoCurrency} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
