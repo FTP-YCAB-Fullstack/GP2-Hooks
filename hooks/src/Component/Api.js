bcbf729cc8135733c6b86c480b7f399748f75eb1
GanXx8 <ganda.imasxx@gmail.com>
2021-09-07 23:10:12 +0700

    Done Styling page MVP

--- hooks/src/Component/Api.js
+++ hooks/src/Component/Api.js
@@ -3,6 +3,7 @@ import React , {useEffect , useState} from 'react'
 import '../currency.css'
 import Logoconvert from "../assets/logoconverts.png"
 import Footer from "./Footer"
+import back2 from "../assets/bck2.png"
 
 function Api() {
     const [useData,setData] = useState([])
@@ -44,15 +45,13 @@ function Api() {
         <div className="bodyconverter">
         <div class="body">
                 <div className="titleconvert">
-                    <h1>Currency Exchange</h1>  
+                    <h1>Currency Exchange</h1>       
                 </div>
-
             <div className="mainconvert">
                 <div className="inputform">
                     <div class="contInput">
                         <input  type="number" value={third} onChange={e => setThird(e.target.value)} placeholder="Input number "></input>
                    </div>
-                   
                  <div className="option">
                    <div class="optionmenu1">
                         <span>From </span>
@@ -68,28 +67,35 @@ function Api() {
                     </div>
                  </div>
                 </div>
-
-                <div className="panah">
-                    <img src={Logoconvert} alt=""/>
-                </div>
-
-                <div className="formresult">
-                    <div className="note">
-                        <h6>Result Exchange</h6>
-                        <span>note : </span>
+                    <div className="panah">
+                        <img src={Logoconvert} alt=""/>
                     </div>
-                    <div className="pembungkus">
-                        <div className="boxresult">
-                            <p class="hasil">{third} {first}= {rate * +third} {second}</p>
+                    <div className="formresult">
+                        <div className="note">
+                            <h6>Result Exchange</h6>
+                            <span>note : </span>
+                        </div>
+                        <div className="pembungkus">
+                            <div className="boxresult">
+                                <p class="hasil">{third} {first}= {rate * +third} {second}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-        </div>
+                    
                 <div className="buttonsub">
-                    <button class="button" onClick={()=> {getRate(first,second)}}> Convert </button>
+                    <button class="btn1" onClick={()=> {getRate(first,second)}}> Convert </button>
                 </div>
         </div>
+        
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d3d3d3" fill-opacity="1" d="M0,224L48,192C96,160,192,96,288,101.3C384,107,480,181,576,218.7C672,256,768,256,864,240C960,224,1056,192,1152,192C1248,192,1344,224,1392,240L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+            </div>
+            <div className="body2">
+                <h1>Start learning about digital money maturity</h1>
+                <p>Are you looking for the price of a crypto? You can see the crypto list on the next page</p>
+                <img src={back2} alt="" width="350px"/> 
+            </div>
         <Footer />
     </div>
     )
